4d02fc1d274ff8d9042fe6277013b734edcd4f0f
opilane <arvikadak@gmail.com>
2024-05-03 12:42:22 +0300

    hui

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,1 @@
-﻿namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(i);
-            }
-        }
-    }
-}
+﻿
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -1;
+            if (a < 0)
+            {
+                Console.WriteLine("a is negative.");
+            }
+            else
+            {
+                Console.WriteLine("a is positive.");
+            }
+        }
+    }
+}
