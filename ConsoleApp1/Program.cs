e2dba0717299fa310d3832cab99655a74c72883a
opilane <arvikadak@gmail.com>
2024-05-03 12:19:13 +0300

    jj

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
